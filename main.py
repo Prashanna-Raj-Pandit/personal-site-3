17d3c4c885db3febfcf3cdce3d21ccc5146fd9f0
Prashanna Raj Pandit <erprp99@gmail.com>
2024-05-11 21:14:15 +0545

    third commit

--- main.py
+++ main.py
@@ -5,5 +5,9 @@ app=Flask(__name__) # name of current directory
 def home():
     return render_template("index.html")
 
+@app.route('/skills')
+def skills():
+    return render_template("skills.html")
+
 if __name__=="__main__":
     app.run(debug=True)
